0b083fa63a4f85e1235f24ee59a8fd821a89b9ed
Franciszek Juras <franciszekjuras@gmail.com>
2021-11-16 13:19:18 +0100

    Foundation

---
+++ localpkgs.py
@@ -0,0 +1,10 @@
+from ntpath import join
+from os.path import dirname, join, realpath
+from os import walk
+import sys
+parent_dir =dirname(dirname(realpath(__file__)))
+prll_dir_names = next(walk(parent_dir))[1]
+prll_dirs = [join(parent_dir, name) for name in prll_dir_names]
+print(prll_dirs)
+for dir in prll_dirs:
+    sys.path.insert(1, dir)
