4c895079c5fe49ad26c38dad3227cfe5b782c704
Franciszek Juras <franciszekjuras@gmail.com>
2021-12-07 11:52:27 +0100

    Dropped autoreg for filtering

--- process.py
+++ process.py
@@ -5,6 +5,8 @@ import argparse
 import matplotlib.pyplot as plt
 import scipy.signal as dsp
 from labpy.series import Series
+import labpy.utils as utils
+from statsmodels.tsa.ar_model import AutoReg
 
 def main(args):
     with open(args.file, "rb") as f:
@@ -12,28 +14,46 @@ def main(args):
     sers = data["data"]
     if args.params: print(data["params"])
     if args.settings: print(data["settings"])
+    sens = utils.str_to_value(data['settings']['lockin']['sensitivity'])
+    
+    tau = 5e-3
+    dmx: Series = (sers['x'] * sens).slice(-tau)
+    cut = 5e-3
+    # dmx.project(cut, 5e-4)
+    # sers['back_proj'] = dmx.copy_y()
 
-    mon = sers["mon1"] + sers["mon2"].y
+    ker = dsp.firwin(int(2 * tau // dmx.dx), 100, fs=dmx.freq, scale=True)
+    kerbp = dsp.firwin(int(2 * tau // dmx.dx), (300, 500), fs=dmx.freq, pass_zero=False)
+    kerbp2 = dsp.firwin(int(2 * tau // dmx.dx), (200, 600), fs=dmx.freq, pass_zero=False)
+    sers['filter'] = Series(*dsp.freqz(kerbp, worN=int(1e4), fs=dmx.freq)[::-1]).slice(0, 1e3)
+
+    mon = sers["mon1"] + sers["mon2"]
     mon = mon.slice(0) - np.mean(mon.slice(r=0).y)
-    mon_org = mon.copy_y()
-    sers['mon_org'] = mon_org
-    tau = 10e-3
-    ker = dsp.firwin(int(2 * tau // mon.dx), 200, fs=mon.freq, scale=True)
-    sers['ker'] = Series(*dsp.freqz(ker, worN=int(1e4), fs=mon.freq)[::-1]).slice(0, 1e3)
     mon.y = dsp.convolve(mon.y, ker, mode='same')
     mon = mon.slice(tau, -tau, rel=True)
     mon_poly = np.polynomial.Polynomial.fit(*mon.xy, deg=2)
-    monfit = Series(mon_poly(mon_org.x), mon_org.x)
+    monfit = Series(mon_poly(dmx.x), dmx.x)
+    dmx /= monfit
     sers['monfit'] = monfit
 
+    x_sta = dmx.filter(ker)
+    # x_sta.project(cut)
+    # x_sta_poly = np.polynomial.Polynomial.fit(*x_sta.slice(tau, 3*tau).xy, deg=2)
+    # x_sta_beg = x_sta.slice(0, tau)
+    # x_sta_beg.y[:] = x_sta_poly(x_sta_beg.x)
+    
+    sers['x_static'] = x_sta
+    # sers['x1'] = dmx - x_sta
+    sers['x'] = dmx.filter(kerbp)
+    # sers['x2'] = dmx.filter(kerbp2)
     sers['mon'] = mon
 
     fig, _ = plt.subplots(3)
     axs = fig.axes
     fig.set_tight_layout(True)
-    for k, i in (('x', 0),('mon', 1),('monfit', 1)):
+    for k, i in (('x', 0),('x_static', 0),('mon', 1),('monfit', 1)):
         axs[i].plot(*sers[k].xy)
-    axs[2].plot(*sers['ker'].abs().xy)
+    axs[2].plot(*sers['filter'].abs().xy)
     # axs[2].loglog(*sers['x'].slice(0, 0.1).fft().abs().xy)
     plt.show()
 
