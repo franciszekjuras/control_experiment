1708484adab73d7ab91f5c79f3c3f364dda0c461
Franciszek Juras <franciszekjuras@gmail.com>
2021-12-15 11:40:08 +0100

    Finished restructuring core. Not tested

--- core.py
+++ core.py
@@ -11,21 +11,21 @@ from datetime import datetime
 import constants
 import settings
 from labpy.devices import daqmx, arduinopulsegen, keithley_cs, srs, tb3000_aom_driver, wavemeter
-from labpy.types import Series, Average, NestedDict
+from labpy.types import Series, Average, NestedDict, DataList
 
 class Core:
-    def __init__(self, settings):
-        self.rm = pyvisa.ResourceManager()
-        self.s = NestedDict(settings)
-        self.base_settings = self.s.copy()
+    def __init__(self, settings, rm = None):
+        self.rm = rm if rm is not None else pyvisa.ResourceManager()
+        self._s = NestedDict(settings)
+        # self.base_settings = self._s.copy()
         self.params = {}
-        self.init_devices()
+        self._init_devices()
         time.sleep(0.1)
 
-    def init_devices(self):
-        self.daq = daqmx.DAQmx(**self.s['daq'])
+    def _init_devices(self):
+        self.daq = daqmx.DAQmx(**self._s['daq'])
 
-        timing = self.s['timing']
+        timing = self._s['timing']
         self.pulsegen = arduinopulsegen.ArduinoPulseGen(
             self.rm, portmap=constants.arduino.portmap,  **timing)
         self.pulsegen.xon(constants.arduino.reversed_polarity)
@@ -35,14 +35,14 @@ class Core:
         for k, v in pulses.items():
             self.pulsegen.xadd(k, v)
 
-        self.lockin = srs.Srs(self.rm, auxout_map=constants.lockin.auxout, **self.s['lockin'])
-        for k, v in self.s['lockin'].get('auxout', {}).items():
+        self.lockin = srs.Srs(self.rm, auxout_map=constants.lockin.auxout, **self._s['lockin'])
+        for k, v in self._s['lockin'].get('auxout', {}).items():
             self.lockin.auxout(k, v)
 
-        self.curr_src = keithley_cs.KeithleyCS(self.rm, self.s['current_source']['dev'])
+        self.curr_src = keithley_cs.KeithleyCS(self.rm, self._s['current_source']['dev'])
         self.curr_src.set_remote_only()
         self.curr_src.current = 0.
-        self.curr_src.set_sweep(self.s['current_source']['sweep'])
+        self.curr_src.set_sweep(self._s['current_source']['sweep'])
 
         self._dev_set = {'daq': self._daq_set, 'timing': self._pulsegen_set,
                          'lockin': self._lockin_set, 'current_source': self._curr_src_set}
@@ -53,6 +53,11 @@ class Core:
         for path, v in dic:
             dev, path = path[0], path[1:]
             self._dev_set[dev](path, v)
+            self._s[path] = v
+
+    @property
+    def settings(self):
+        return self._s.copy()
 
     def _daq_set(self, path, v):
         raise ValueError(f"Can't set property {path} on daq")
@@ -87,7 +92,7 @@ class Core:
             raise ValueError(f"Can't set property {path} on curr_src")
 
     def _trigger_to_pulse(self, trigs):
-        return sum([[v, v + self.s['timing']['trigger_width']] for v in trigs],[])
+        return sum([[v, v + self._s['timing']['trigger_width']] for v in trigs],[])
 
     def snap_params(self):
         p = {}
@@ -96,48 +101,85 @@ class Core:
         p['lasers'] = {ch + ' freq': v for ch, v in zip(chs, freqs)}
         return p
 
+    @staticmethod
+    def _create_figures(specs:dict={}, grid_specs:dict={}):
+        figs = {}
+        for fig in specs:
+            if fig in grid_specs:
+                grid_spec = grid_specs[fig]
+            else:
+                grid_spec = [max([el[1] for el in specs]) - 1]
+            f, _ = plt.subplots(*grid_spec)
+            f.set_tight_layout(True)
+            figs[fig] = {'fig': f, 'spec': specs[fig]}
+        return figs
 
-def main(args):
-    exec_aux_commands(args)
-    setts = settings.load()
-    core = Core(settings=settings.load())
+    @staticmethod
+    def _zip_scan(scan:dict[list]):
+        scan_list = [{k: v for k, v in zip(scan.keys(), lst)}
+                               for lst in zip(*scan.values())]
+        if len(scan_list) != max([len(el) for el in scan.values()]):
+            raise ValueError("All lists in scan should be of same length")
+        return scan_list
 
-    plt.ion()
-    fig, axs = plt.subplots(2)
-    fig.set_tight_layout(True)
-    axs = fig.axes
-    avgs = [Average() for _ in range(daq.chs_n)]
-    time.sleep(0.1)
-
-    t = np.linspace(daq.t0, daq.time + daq.t0, daq.samples, endpoint=False)
-
-    s["avarages"] = args.repeat
-    for i in range(s["avarages"]):
-        curr_src.init()
-        daq.start()
-        time.sleep(-daq.t0)
-        pulsegen.run()
-        data = daq.read(10)
-        series = from2darray(data, t)
-        for avg, ser in zip(avgs, series):
-            avg.add(ser.y)
+    @staticmethod
+    def _plot(data, fig=None, spec=None):
+        if fig is None or spec is None:
+            return
+        axs = fig.axes
         for ax in axs:
             ax.clear()
-        for i, ser in zip((0,0,1,1,1), series):
-            axs[i].plot(*ser.xy)
+        for id, pos, *opt in spec:
+            opt = opt[0] if len(opt) else {}
+            print(opt)
+            axs[pos].plot(*data[id].xy)
         # axs[1].set_xbound([-0.1e-3, 0.2e-3])
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-    avgs = {k:avg.value for (k, avg) in zip(constants.daq.labels, avgs)}
-    avgser = {key:Series(avg, t) for (key, avg) in avgs.items()}
-    if s["avarages"] != 1:
-        fig, axs = plt.subplots(2)
-        fig.set_tight_layout(True)
-        for i, ser in zip((0,0,1,1,1), avgser.values()):
-            axs[i].plot(*ser.xy)
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+    def run(self, scan:dict[list]=None, plots:dict={}, grid_specs:dict={}):
+        self.result = DataList()
+        if scan is not None:
+            scan_list = Core._zip_scan(scan)
+            self.result.scan = scan
+        else:
+            scan_list = [{}]
+        self.result.settings = self._s.copy()
+        self.result.params = self.snap_params()
+        plt.ion()
+        figs = Core._create_figures(plots, grid_specs).values()
+        t = self.daq.space()
+        avgs = {k: Average() for k, _
+            in zip(constants.daq.labels, range(self.daq.chs_n))}
+
+        for shot_sett in scan_list:
+            entry = {}
+            self.set(shot_sett)
+            entry['settings'] = shot_sett.copy()
+            entry['params'] = self.snap_params()
+            for _ in range(self._s["averages"]):
+                self.curr_src.init()
+                self.daq.start()
+                time.sleep(-self.daq.t0)
+                self.pulsegen.run()
+                data = self.daq.read()
+                series = dict(zip(constants.daq.labels, Series.from2darray(data, t)))
+                for k, ser in series.items():
+                    avgs[k].add(ser)
+                if self._s['averages'] != 1:
+                    Core._plot(series, **figs.get('single', {}))
+            series_avg = {k: v.value() for k, v in avgs.items()}
+            entry.update(series_avg)
+            self.result.append(entry)
+            Core._plot(entry, **figs.get('avg', {}))
+
+def main(args):
+    rm = pyvisa.ResourceManager()
+    exec_aux_commands(args, rm)
+    setts = settings.load()
+    setts['averages'] = args.repeat
+    core = Core(settings=setts, rm=rm)
+    core.run()
 
     if args.save is not None:
         savepath = Path("data/" + args.save.strip("/\\") + '/' + "SINGLE"
@@ -145,11 +187,11 @@ def main(args):
         print(savepath.parent)
         savepath.parent.mkdir(exist_ok=True, parents=True)
         with savepath.open("wb") as f:
-            pickle.dump({"data": avgser, "params": params, "settings": s}, f)
+            pickle.dump(core.result, f)
 
     input("Press enter to exit...")
 
-def exec_aux_commands(args):
+def exec_aux_commands(args, rm):
     exit = False
     if args.list:
         exit = True
@@ -159,7 +201,7 @@ def exec_aux_commands(args):
             print(f"{el[0]:>15}  {el[1]}")
     if args.aom:
         exit = True
-        pulsegen = ArduinoPulseGen(rm, "Arduino", portmap=constants.arduino.portmap)
+        pulsegen = arduinopulsegen.ArduinoPulseGen(rm, "Arduino", portmap=constants.arduino.portmap)
         pulsegen.xon(("probeEn"))
     if exit:
         sys.exit(0)
