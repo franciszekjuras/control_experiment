cb8177cfd1cc8ae4b402f01d1c89ae16e0ca5519
Franciszek Juras <franciszekjuras@gmail.com>
2022-01-29 16:36:35 +0100

    Added fitting models and bounds calculation

--- model.py
+++ model.py
@@ -12,7 +12,9 @@ class Model:
         self._meta = data.meta
         self._settings = NestedDict(data.settings)
         self._idx = idx
-        self._bounds = bounds
+        self.bounds = bounds
+        '''dict: Bounds for fit parameters. Estimated from data if not specified,
+        except decay parameters `gr`, `g1` and `g2`'''
         self._v = verbose
         self.hyper_params = {'osc_freq_window': 0.1, 'filter_atten_dB': 52.}
         self.result = []
@@ -44,9 +46,39 @@ class Model:
         rotlp = dsp.filter(rot, kerlp).cut(dead_time, -dead_time)
         rotbp = dsp.filter(rot, kerbp).cut(dead_time, -dead_time)
         if idx + '_norm' in shot:
+            # Filter normalization data and normalize rotlp and rotbp
             print("Normalization not implemented. Normalization data ignored.")
         else:
             if self._v: print('No normalization data. Fitting to raw signal.')
+        fit_bp_res = self._fit_bp(rotbp, osc_freq)
+
+    def _fit_bp(self, rot, osc_freq):
+        bounds = self._bounds_bp(rot, osc_freq)
+        pass
+
+    def _bounds_bp(self, rot, osc_freq):
+        bounds = self.bounds.copy()
+        if 'r' not in bounds:
+            ptp = np.max(rot.y) - np.min(rot.y)
+            bounds['r'] = [0, ptp]
+        if 'f' not in bounds:
+            bounds['f'] = [osc_freq * (1 + s * self.hyper_params['osc_freq_window']) for s in [-1, 1]]
+        if 'ph' not in bounds:
+            bounds['ph'] = [-2 * np.pi, 2 * np.pi]
+        return [bounds[idx] for idx in self.params[0:4]]
+
+    def _bounds_lp(self, rot):
+        bounds = self.bounds.copy()
+        maxv, minv =  np.max(rot.y), np.min(rot.y)
+        ptp = maxv - minv
+        if 'c1' not in bounds:
+            bounds['c1'] = [-4* ptp, 4* ptp]
+        if 'c2' not in bounds:
+            bounds['c2'] = [-4* ptp, 4* ptp]
+        if 'off' not in bounds:
+            bounds['off'] = [rot.y[-1] - ptp, rot.y[-1] + ptp]
+        return [bounds[idx] for idx in self.params[4:9]]
+
 
     def _calc_filter_kernels(self, samp_freq, osc_freq):
         nyq_freq = samp_freq/2.
@@ -73,6 +105,51 @@ class Model:
             if self._v: print("No frequency peak found")
         return osc_freq
 
+def chi_squared_gen(model):
+    return lambda params, x, y: np.sum(np.square(model(x, *params) - y))
 
-    def fit(self):
-        rot = Series(self._data[self._idx])
+def model_bp(x, r, gr, f, ph):
+    return r * np.sin(2*np.pi*f * x + ph) * np.exp(-gr * x)
+
+class model_lp_gen:
+    def __init__(self, slow_decay: bool = True, fast_decay: bool = True, offset: float = None):
+        self._fast_decay = bool(fast_decay)
+        self._slow_decay = bool(slow_decay)
+        self._offset = offset
+
+    def strip_params(self, params):
+        p = list(params)
+        i = 0
+        for decay in [self._slow_decay, self._fast_decay]:
+            if not decay:
+                del p[i:i+2]
+            else:
+                i += 2
+        if self._offset is not None:
+            del p[-1:]
+        return p
+
+    def full_params(self, params):
+        p = []
+        for decay in [self._slow_decay, self._fast_decay]:
+            if decay:
+                c, g, *params = params
+                p += [c, g]
+            else:
+                p += [0., 0.]
+        if self._offset is None:
+            c, = params
+            p += [c]
+        else:
+            p += [self._offset]
+        return p
+
+    def __call__(self, *args):
+        x, *params = args
+        c1, g1, c2, g2, off = self.full_params(params)
+        res = np.full_like(x, off, dtype=np.float64)
+        if c1 != 0.:
+            res += c1 * np.exp(-g1 * x)
+        if c2 != 0.:
+            res += c2 * np.exp(-g2 * x)
+        return res
